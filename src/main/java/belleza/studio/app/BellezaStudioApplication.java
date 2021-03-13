3bc989e39f7d604ceb8953ef3a1569818da5c140
madsteve32 <stivan_lyutov@abv.bg>
2021-03-13 16:33:41 +0200

    Finalizing settings for project
    Make some changes in images and application property.

---
+++ src/main/java/belleza/studio/app/BellezaStudioApplication.java
@@ -0,0 +1,13 @@
+package belleza.studio.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellezaStudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BellezaStudioApplication.class, args);
+    }
+
+}
