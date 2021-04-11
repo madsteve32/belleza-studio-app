5b0f9b588cf2202a5004f55d46075fd0a8f24190
madsteve32 <stivan_lyutov@abv.bg>
2021-04-11 12:33:24 +0300

    Booking an hour test
    Adding test for booking an hour from a client.

--- src/test/java/belleza/studio/app/web/BookedHoursControllerTest.java
+++ src/test/java/belleza/studio/app/web/BookedHoursControllerTest.java
@@ -1,5 +1,7 @@
 package belleza.studio.app.web;
 
+import belleza.studio.app.repositories.BookedHoursRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -9,6 +11,9 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.time.LocalDateTime;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -17,6 +22,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class BookedHoursControllerTest {
 
     private static final String BOOKED_HOURS_CONTROLLER_PREFIX = "/booking";
+
+    @Autowired
+    private BookedHoursRepository bookedHoursRepository;
 
     @Autowired
     private MockMvc mockMvc;
@@ -28,5 +36,19 @@ public class BookedHoursControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("booked-hours-list"))
                 .andExpect(model().attributeExists("bookedHours"));
+    }
+
+    @Test
+    void bookAnHour() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post(BOOKED_HOURS_CONTROLLER_PREFIX + "/hour")
+        .param("firstName", "Test")
+        .param("lastName", "Testerov")
+        .param("bookedDateAndTime", "2021-04-27T12:00")
+        .param("number", "0898786534")
+        .param("serviceType", "Haircut")
+        .with(csrf()))
+                .andExpect(status().is3xxRedirection());
+
+        Assertions.assertEquals(1, bookedHoursRepository.count());
     }
 }
